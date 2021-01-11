2cfc5d5e27f425bcbf0029c05458dd2d9e43d856
Jonsy13 <vedant.shrotria@mayadata.io>
2021-01-11 14:53:16 +0530

    Updated Script for getting frontend IP.
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>

--- k8s_scripts/install.sh
+++ k8s_scripts/install.sh
@@ -24,6 +24,19 @@ kubectl get svc -n kubera
 # Waiting for pods to be ready
 kubectl wait --for=condition=Ready pods --all --namespace kubera --timeout=120s
 
+# Getting The LoadBalancer IP for accessing Kubera-core
+kubectl patch svc kubera-ingress-nginx-controller -p '{"spec": {"type": "LoadBalancer"}}' -n kubera
+IP=$(kubectl get svc -n kubera --no-headers |awk '/kubera-ingress-nginx-controller/ {print $4}' | head -n 1)
+URL=http://$IP
+
+# Waiting for URL to be active
+until $(curl --output /dev/null --silent --head --fail $URL); do
+    printf '.'
+    sleep 5
+done
+
+echo "URL to access Kubera-Core: $URL"
+
 # # Getting latest kubeconfig in cache
 cat ~/.kube/config > $path/.kube/config
 cat ~/.kube/config > $path/.kube/admin.conf
